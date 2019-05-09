acc25f6224daac3b32cef34932cb7e4fcd0304c1
fbgian <fgchaka@gmail.com>
2019-05-09 22:43:15 +0200

    Test percentuale batteria

--- app/src/main/java/com/fabanto/batteryshow/Sketch.java
+++ app/src/main/java/com/fabanto/batteryshow/Sketch.java
@@ -1,6 +1,28 @@
 package com.fabanto.batteryshow;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.BatteryManager;
+import android.util.Log;
+import android.widget.TextView;
+
 import processing.core.PApplet;
 
 class Sketch extends PApplet {
+
+    public void draw() {
+
+        BroadcastReceiver mBatInfoReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                int level = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, 0);
+                Log.d("livello", String.valueOf(level) + "0");
+            }
+        };
+        //battery = (TextView) this.findViewById(R.id.text);
+        getActivity().registerReceiver(mBatInfoReceiver, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
+
+    }
 }
