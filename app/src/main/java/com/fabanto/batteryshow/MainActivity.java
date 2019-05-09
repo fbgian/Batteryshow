e59c826d36671c87bb06ebcf8d912f5bc578b0ed
antlanc7 <antonio.lanciotti@live.it>
2019-05-09 22:18:28 +0200

    aggiunte procedure processing

--- app/src/main/java/com/fabanto/batteryshow/MainActivity.java
+++ app/src/main/java/com/fabanto/batteryshow/MainActivity.java
@@ -1,13 +1,40 @@
 package com.fabanto.batteryshow;
 
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import processing.android.PFragment;
+
 public class MainActivity extends AppCompatActivity {
+
+    private Sketch sketch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+        sketch = new Sketch();
+        PFragment fragment = new PFragment(sketch);
+        fragment.setView(findViewById(R.id.constraintLayout), this);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (sketch != null) {
+            sketch.onRequestPermissionsResult(
+                    requestCode, permissions, grantResults);
+        }
+    }
+
+    @Override
+    public void onNewIntent(Intent intent) {
+        if (sketch != null) {
+            sketch.onNewIntent(intent);
+        }
     }
 }
